64d811b8fc63c6c33ae2deb23a769a5c7921ba51
habib-sc <habib97.cs@gmail.com>
2022-04-25 17:40:54 +0600

    added access token with jwt and gettigs users orders

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+var jwt = require('jsonwebtoken');
 
 // middleware 
 app.use(cors());
@@ -22,6 +23,15 @@ async function run () {
         await client.connect();
         const serviceCollection = client.db("CarService").collection("services");
         const orderCollection = client.db("CarService").collection("orders");
+
+        // Auth token 
+        app.post('/login', async(req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({accessToken});
+        });
 
         // service get 
         app.get('/services', async(req, res) => {
@@ -57,10 +67,20 @@ async function run () {
 
         // =================== Order collection =====================
 
+        // posting order 
         app.post('/order', async(req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
+        });
+
+        // getting orders 
+        app.get('/orders', async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
         });
 
     }
